972a6a2b7c6fe7eef820aa129a9a953667a409fa
basedtr <70116305+basedtr@users.noreply.github.com>
2022-07-06 00:34:21 -0400

    Birthday planner working

--- birthday-planner/setup/src/data.js
+++ birthday-planner/setup/src/data.js
@@ -34,4 +34,12 @@ export default [
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
+  {
+    id: 6,
+    name: 'Sharon Rodriguez',
+    age: 23,
+    image:
+      'https://scontent.ftpa1-1.fna.fbcdn.net/v/t39.30808-6/218239855_10223367225882323_869270266486015245_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=wfAgR7oU1bYAX9UEfQD&tn=6_IVEPMG3KgosC1E&_nc_ht=scontent.ftpa1-1.fna&oh=00_AT8YYmD3Ln215qhT77zOnpF7f3GZmGFQh8jpF9cQQ4ocyA&oe=62CA0024',
+  },
+  
 ];
