149672e1d5f6dcded48f3dd5589194043417cfb9
basedtr <70116305+basedtr@users.noreply.github.com>
2022-07-12 22:23:51 -0400

    added reviews component

--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -3,7 +3,31 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(4);
+  const {name, job, image, text} = people[index] //as we change the value of the state of index, we can change which person is being shown
+  return <article className='review'>
+    <div className="img-container">
+      <img src={image} alt={name} className='person-img' />
+      <span className='quote-icon'>
+        <FaQuoteRight />
+      </span>
+    </div>
+    <h4 className='author'>{name}</h4>
+    <p className='job'>{job}</p>
+    <p className='info'>{text}</p>
+    <div className="button-container">
+      <button className='prev-btn'>
+        <FaChevronLeft />
+      </button>
+      <button className='next-btn'>
+        <FaChevronRight />
+      </button>
+      
+    </div>
+    <button className='random-btn'>
+        Random Review
+    </button>
+  </article>;
 };
 
 export default Review;
--- reviews/src/data.js
+++ reviews/src/data.js
@@ -35,6 +35,15 @@ const reviews = [
     text:
       'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
   },
+  {
+    id: 5,
+    name: 'sharon rodriguez',
+    job: 'the girlfriend',
+    image:
+      'https://scontent.ftpa1-2.fna.fbcdn.net/v/t39.30808-6/293463414_10225332476292355_7076211442172358797_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=730e14&_nc_ohc=gD58MScplDUAX9NYJ-K&_nc_ht=scontent.ftpa1-2.fna&oh=00_AT-z2Yv44fxAmnyv3Vyxg4nhJXpXZxo4o78bDdZgvPzylw&oe=62D337B0',
+    text:
+      'spends my money but is the best girlfriend ever. ',
+  },
 ];
 
 export default reviews;
