2cb6a2c1f17bec6e2ef86ec3b51dc003fb9a4cda
basedtr <70116305+basedtr@users.noreply.github.com>
2022-07-19 15:39:59 -0400

    menu complete, filter works dynamically instead of manually

--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({categories, filterItems}) => {
+
+  return (
+  <div className='btn-container'> 
+    {categories.map((category, index)=>{  //this is how we will create buttons dynamically; can add/remove categories later
+      return(
+      <button type="button"
+      className="filter-btn"
+      key={index}
+      onClick={()=> filterItems(category)}>
+        {category}
+      </button>
+      );
+    })}
+    
+  </div>
+  );
 };
 
 export default Categories;
