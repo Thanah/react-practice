3117dc7dc8c02518b797e5bc312bee934ac8d899
basedtr <70116305+basedtr@users.noreply.github.com>
2022-07-26 19:47:07 -0400

    completed slide movement and auto scroll

--- sliders/src/App.js
+++ sliders/src/App.js
@@ -5,6 +5,23 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+  
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if(index < 0){
+      setIndex(lastIndex);
+    }
+    if(index > lastIndex){
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect( () =>{
+    let slider = setInterval(()=>{  
+      setIndex(index + 1)
+    }, 3000); //3 second interval
+    return () => clearInterval(slider); //clean up function
+  }, [index]); //invoked whenever the index changes
   return <section className="section">
     <div className="title">
       <h2>
@@ -14,8 +31,15 @@ function App() {
     <div className="section-center">
       {people.map((person, personIndex)=>{
           const {id, image, name, title, quote} = person; //deconstruct data
-          //TODO more changes
-          return (<article key = {id}>  
+          //TODO more classes
+          let position = 'nextSlide';
+          if (personIndex === index){ //if the person is the current index, it becomes active
+            position = 'activeSlide';
+          }
+          if(personIndex === index - 1 || (index === 0 && personIndex === person.length - 1)){  //for the prev person OR the last person if curr is first person
+            position = 'lastSlide'
+          }
+          return (<article className={position} key = {id}>  
             <img src={image} alt={title} className="person-img"/>
             <h4>{name}</h4>
             <p className="title">{title}</p>
@@ -23,10 +47,10 @@ function App() {
             <FaQuoteRight className='icon' />
           </article>);
         })}
-        <button className="prev">
+        <button className="prev" onClick={()=> setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next">
+        <button className="next" onClick={()=> setIndex(index + 1)}>
           <FiChevronRight />
         </button>
     </div>
